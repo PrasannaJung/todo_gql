dbf5fe7fe46a95aed2669a49ebe6013c0816c88a
PrasannaJung <prasannajung08@gmail.com>
2025-07-30 21:13:49 +0545

    Added user creation and fetching api

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,5 +1,25 @@
-import { Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Resolver } from '@nestjs/graphql';
 import { UserEntity } from './entity/user.entity';
+import { Query } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { CreateUserInput } from './dto/create-user.dto';
 
-@Resolver()
-export class UserResolver {}
+@Resolver(() => UserEntity)
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @Query(() => [UserEntity], { name: 'users' })
+  getAllUsers() {
+    return this.userService.getAllUsers();
+  }
+
+  @Query(() => UserEntity, { name: 'me' })
+  getUser(@Args('id', { type: () => Int }) id: number) {
+    return this.userService.getUserById(id);
+  }
+
+  @Mutation(() => UserEntity)
+  createUser(@Args('input') input: CreateUserInput) {
+    return this.userService.createUser(input);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,21 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { UserEntity } from './entity/user.entity';
 import { users as USERS_DATA } from './store/user.store';
+import { CreateUserInput } from './dto/create-user.dto';
 
+@Injectable()
 export class UserService {
   private readonly users: UserEntity[] = USERS_DATA;
+
+  createUser(input: CreateUserInput) {
+    const newUser = { id: this.users.length + 1, ...input };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  getAllUsers() {
+    return this.users;
+  }
 
   getUserById(id: number): UserEntity {
     const user = this.users.find((user) => user.id === id);
